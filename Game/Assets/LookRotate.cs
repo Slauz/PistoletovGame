7e22349eee89b22e4145ba8c1876664556ad519a
Artem Ostrizhnev <ostrizhnevartem@gmail.com>
2023-05-20 21:59:17 +0300

    v0.1.0
    First commit.
    - Movement.
    - Some terrain.

---
+++ Game/Assets/LookRotate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookRotate : MonoBehaviour
+{
+    public float sensivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 10f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);   
+    }
+}
